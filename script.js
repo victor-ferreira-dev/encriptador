bad5c29a18cb1a8ed4970d3f59dc8cba7e83eb2a
Victor Ferreira <118637092+victor-ferreira-dev@users.noreply.github.com>
2023-06-13 11:21:39 -0300

    debug & refactoring all files

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// DOM elements
+// DOM elements from index.html
     const $textCript = document.querySelector(".text-cript");
-        const tcPlaceholder = $textCript.placeholder;
 
     const $btnCript = document.querySelector(".btn-cript");
+        const $spanCript = document.querySelector(".btn-cript span");
     const $btnDescript = document.querySelector(".btn-descript");
+        const $spanDescript = document.querySelector(".btn-descript span");
 
     const $containerWithoutMsg = document.querySelector('.container-withoutMsg');
 
@@ -12,21 +13,31 @@
     const $btnCopy = document.querySelector('.btn-copy');
         const $iCopy = document.querySelector('#copy');
         const $iCheck = document.querySelector('#check');
-        const $span = document.querySelector(".btn-copy span");
+        const $spanCopy = document.querySelector(".btn-copy span");
     
-// Interactions with DOM
+// DOM defaults
     $textDescript.style.display = 'none';
     $btnCopy.style.display = 'none';
     $iCheck.style.display = 'none';
 
-// (i) - 0 --- 1
-// 0 - "e" => "enter"
-// 1 - "i" => "imes"
-// 2 - "a" => "ai"
-// 3 - "o" => "ober"
-// 4 - "u" => "ufat"
-    // "gato" => "gaitober"
-    // gaitober" => "gato"
+// Variables with original text values
+    const origPhCript = $textCript.placeholder; // Digite seu texto
+    const origBtnCript = $spanCript.textContent; // Criptografar
+    const origBtnDescript = $spanDescript.textContent; // Descriptografar
+    const origBtnCopy = $spanCopy.textContent; // Copiar
+    
+// Timeout ranges
+    const outCript = 1000; // Faster
+    const outCopy = 2000; // Medium
+    const outDelay = 10000; // Larger
+
+/* (i) -0- -|- -1-
+** -0- "e" => "enter"
+** -1- "i" => "imes"
+** -2- "a" => "ai"
+** -3- "o" => "ober"
+** -4- "u" => "ufat" 
+** For studying purposes */
     const criptMatrix = [
         ['e' , 'enter'], 
         ['i', 'imes'], 
@@ -34,88 +45,9 @@
         ['o', 'ober'],
         ['u', 'ufat']
     ];
-        console.table(criptMatrix);
+        // console.table(criptMatrix); // Only for test purposes
 
-// Criptografar
-    function cript(textCript) {
-        if ($textCript.value.length > 0) {
-            textCript = $textCript.value.toLowerCase();
-            const originalPh = tcPlaceholder;
-    
-            for (let i = 0; i < criptMatrix.length; i++) {
-                if (textCript.includes(criptMatrix[i][0])) {
-                    textCript = textCript.replaceAll(criptMatrix[i][0], criptMatrix[i][1]);
-                }
-                $textDescript.value = textCript;
-            }
-    
-            $textCript.value = ""; // Clean input
-            tcPlaceholder.textContent = "Criptografado!";
-    
-            setTimeout(() => {
-                tcPlaceholder.textContent = originalPh;
-            }, 2000);
-            
-        } else if ($textCript.required) {
-            alert("Preencha o campo!");
-        }
-    };
-
-// Descriptografar
-    function descript(textDescript) {
-        textDescript = $textCript.value.toLowerCase();
-        for (let i = 0; i < criptMatrix.length; i++) {
-            if (textDescript.includes(criptMatrix[i][1])) {
-                textDescript = textDescript.replaceAll(criptMatrix[i][1], criptMatrix[i][0]);
-            }
-            $textDescript.value = textDescript;
-        }
-        $textCript.value = "";
-    };
-
-// Shows the $containerWithoutMsg while length of $textCript is 0
-    function hideWithoutMsg() {
-        if ($textDescript.value.length > 0) {
-            $textDescript.style.display = '';
-            $btnCopy.style.display = '';
-            $containerWithoutMsg.style.display = 'none';
-        } else {
-            $textDescript.style.display = 'none';
-            $btnCopy.style.display = 'none';
-            $containerWithoutMsg.style.display = '';
-        }
-        $textCript.value = '';
-    };
-
-// $btnCopy => copy the text on $textDescript to the clipboard
-    function copy() {
-        navigator.clipboard.writeText($textDescript.value);
-
-        const originalText = $span.textContent;
-        $iCopy.style.display = 'none';
-        $span.textContent = 'Copiado!';
-        $iCheck.style.display = '';
-
-        setTimeout(() => {
-            $span.textContent = originalText;
-            $iCheck.style.display = 'none';
-            $textDescript.value = '';
-
-            $textDescript.style.display = 'none';
-            $btnCopy.style.display = 'none';
-            $containerWithoutMsg.style.display = '';
-            $iCopy.style.display = ''; // (ver se tá bugado com o $containerWithoutMsg)
-        }, 2000);
-
-        console.log('Copied to clipboard: ' + $textDescript.value); // Only for test purposes
-    }
-
-// Show container when click on $btnCopy and clear $textDescript
-    // function showContainer () {
-    //     $btnCopy.addEventListener('click', hideWithoutMsg);
-    // };
-
-// Deletes accent letters on $textCript
+// Deletes accent letters on input $textCript area
     $textCript.addEventListener('input', function() {
         this.value = this.value.replace(/[éèêë]/gi, 'e')
                                 .replace(/[áàãâä]/gi, 'a')
@@ -124,16 +56,127 @@
                                 .replace(/[úùûü]/gi, 'u')
     });
 
-// Only for test purposes
-    const delay = 500;
-    let timeoutId;
+// Onclick $btnCript => criptografar & shows text on $textCript
+    function cript(textCript) {
+        if ($textCript.value.length > 0) {
+            textCript = $textCript.value.toLowerCase();
+    
+            $textCript.value = ""; // Clean input $textCript area
+            $textCript.placeholder = "Aguarde...";
+            $spanCript.textContent = "Criptografando...";
 
-    $textCript.addEventListener('input', function() {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(function() {
-            if ($textCript.value.length > 0) {
-                console.log($textCript.value);
+            // Loop through criptMatrix (Encrypt)
+            for (let i = 0; i < criptMatrix.length; i++) {
+                if (textCript.includes(criptMatrix[i][0])) {
+                    textCript = textCript.replaceAll(criptMatrix[i][0], criptMatrix[i][1]);
                 }
-            }, delay);
+                $textDescript.value = textCript;
+            }
+
+            // Whatever button click => 1.25sec to shows the $textDescript & hide $containerWithoutMsg
+            // $btnCopy click => 2sec to show $containerWithoutMsg & return all originals
+            // None click on $bntCopy => 15sec to return all originals
+
+        } else if ($textCript.required || $textCript.value === "") {
+            alert("Preencha o campo com o texto para criptografar!");
         }
-    );
+    };
+
+// Onclick $btnDescript => descriptografar & shows text on $textDescript
+    function descript(textDescript) {
+        if ($textCript.value.length > 0) {
+            textDescript = $textCript.value.toLowerCase();
+
+            $textCript.value = ""; // Clean input $textCript area
+            $textCript.placeholder = "Aguarde...";
+            $spanDescript.textContent = "Descriptografando...";
+            
+            for (let i = 0; i < criptMatrix.length; i++) {
+                if (textDescript.includes(criptMatrix[i][1])) {
+                    textDescript = textDescript.replaceAll(criptMatrix[i][1], criptMatrix[i][0]);
+                }
+                $textDescript.value = textDescript;
+            }
+
+            // Whatever button click => 1.25sec to shows the $textDescript & hide $containerWithoutMsg
+            // $btnCopy click => 2sec to show $containerWithoutMsg & return all originals
+            // None click on $bntCopy => 15sec to return all originals
+
+        } else if ($textCript.required || $textCript.value === "") {
+            alert("Preencha o campo com o texto para descriptografar!");
+        }
+    };
+
+// Onclick $btnCopy => copy the text generated on $textDescript to clipboard
+    function copy() {
+        navigator.clipboard.writeText($textDescript.value); // Copy the text to clipboard
+
+        $iCopy.style.display = 'none'; // Hide the copy icon
+        $iCheck.style.display = ''; // Shows the check icon
+        $spanCopy.textContent = 'Copiado!';
+
+        console.log('Copied to clipboard: ' + $textDescript.value); // Only for test purposes
+    }
+
+// $btnCript on click => hide $containerWithoutMsg
+    $btnCript.addEventListener('click', () => {
+        if ($textDescript.value.length > 0) {
+            setTimeout(() => {
+                $textCript.placeholder = "Pronto!";
+                $spanCript.textContent = "Criptografado!";
+
+                $textDescript.style.display = '';
+                $btnCopy.style.display = '';
+                $containerWithoutMsg.style.display = 'none'; // Hide $containerWithoutMsg
+            }, outCript); // Shows the $textDescript readonly and $btnCopy
+        }
+    });
+
+// $btnDescript on click => hide $containerWithoutMsg
+    $btnDescript.addEventListener('click', () => {
+        if ($textDescript.value.length > 0) {
+            setTimeout(() => {
+                $textCript.placeholder = "Pronto!";
+                $spanDescript.textContent = "Descriptografado!";
+
+                $textDescript.style.display = '';
+                $btnCopy.style.display = '';
+                $containerWithoutMsg.style.display = 'none'; // Hide $containerWithoutMsg
+            }, outCript); // Shows the $textDescript readonly area and $btnCopy
+        }
+    });
+
+// $btnCopy on click => hide $containerWithoutMsg
+    $btnCopy.addEventListener('click', () => {
+        if ($textDescript.value.length > 0) {
+            setTimeout(() => {
+                $textDescript.value = ""; // Clean text readonly area
+
+                $iCheck.style.display = 'none'; // Hide the check icon
+                $iCopy.style.display = ''; // Show the copy icon
+
+                $textCript.placeholder = origPhCript; // Digite seu texto
+                $spanCript.textContent = origBtnCript; // Criptografar
+                $spanDescript.textContent = origBtnDescript; // Descriptografar
+                $spanCopy.textContent = origBtnCopy; // Copiar
+
+                $textDescript.style.display = 'none'; // Hide readonly text area
+                $btnCopy.style.display = 'none'; // Hide $btnCopy
+                $containerWithoutMsg.style.display = ''; // Show $containerWithoutMsg
+            }, outCopy); // Return everything to original
+        }
+    });
+
+// Only for debug & test purposes
+    // const delayTest = 500;
+    // let timeoutId;
+
+    // $textCript.addEventListener('input', function() {
+    //     clearTimeout(timeoutId);
+    //     timeoutId = setTimeout(function() {
+    //         if ($textCript.value.length > 0) {
+    //             console.log($textCript.value);
+    //             }
+    //         }, delayTest);
+    //     }
+    // );
